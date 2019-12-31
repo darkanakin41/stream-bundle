f5bd3f213dbf39c90b37ab45ba9c7dc8a39700c1
Pierre LEJEUNE <darkanakin41@gmail.com>
2019-12-31 11:42:30 +0100

    Upgrading upgrade command

--- src/Command/UpgradeCommand.php
+++ src/Command/UpgradeCommand.php
@@ -13,6 +13,8 @@ use Darkanakin41\StreamBundle\Requester\TwitchRequester;
 use Darkanakin41\StreamBundle\Service\StreamService;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -20,6 +22,7 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 class UpgradeCommand extends Command
 {
     const NB_ITERATION = 10;
+    const NB_PER_ITERATION = 100;
 
     public static $defaultName = 'darkanakin41:stream:upgrade';
 
@@ -54,41 +57,64 @@ class UpgradeCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+
+        $outputStyle = new OutputFormatterStyle('white', 'green', ['bold']);
+        $output->getFormatter()->setStyle("success", $outputStyle);
+
         $output->writeln(array(
             'Darkanakin41 Stream Upgrade',
             '============',
             '',
         ));
+        $synthesis = [];
 
         for ($i = 0; $i < self::NB_ITERATION; ++$i) {
+            $output->writeln(sprintf('[%s] Iteration %d : <info>START</info>', PlatformNomenclature::TWITCH, $i));
             /** @var Stream[] $streams */
-            $streams = $this->managerRegistry->getRepository($this->config['stream_class'])->findBy(array('platform' => PlatformNomenclature::TWITCH, 'userId' => null));
+            $streams = $this->managerRegistry->getRepository($this->config['stream_class'])->findBy(array('platform' => PlatformNomenclature::TWITCH, 'userId' => null), [], self::NB_PER_ITERATION);
             $requester = $this->streamService->getRequester(PlatformNomenclature::TWITCH);
+            $table = new Table($output->section());
+            $table->setHeaders(['Platform', 'Stream', 'Status']);
             foreach ($streams as $stream) {
+                $action = "";
                 try {
+                    if(empty($stream->getIdentifier())){
+                        $this->managerRegistry->getManager()->remove($stream);
+                        $action = '<error>Removed</error>';
+                    }
+
                     /** @var TwitchRequester $requester */
                     $data = $requester->getUserData($stream->getIdentifier());
 
                     if (null === $data) {
                         $this->managerRegistry->getManager()->remove($stream);
-                        $action = 'removed';
+                        $action = '<error>Removed</error>';
                     } else {
                         $stream->setUserId($data['id']);
                         $stream->setIdentifier($data['login']);
                         $this->managerRegistry->getManager()->persist($stream);
-                        $action = 'upgraded';
+                        $action = '<success>Upgraded</success>';
                     }
                 } catch (\Exception $e) {
                     if (false !== stripos($e->getMessage(), 'Invalid login names, emails or IDs in request')) {
                         $this->managerRegistry->getManager()->remove($stream);
-                        $action = 'removed';
+                        $action = '<error>Removed</error>';
                     }
                 }
-                $output->writeln(sprintf('[%s] Stream %s : %s', PlatformNomenclature::TWITCH, $stream->getName(), $action));
+                if(!isset($synthesis[$action])){
+                    $synthesis[$action] = 0;
+                }
+                $synthesis[$action]++;
+                $table->addRow([ucfirst(PlatformNomenclature::TWITCH), $stream->getName(), $action]);
             }
+            $table->render();
             $this->managerRegistry->getManager()->flush();
         }
 
         $output->writeln('');
+
+        $output->writeln('Synthesis');
+        $table->setHeaders(array_keys($synthesis));
+        $table->addRow(array_values($synthesis));
     }
 }
